7172e7c0b36471fdc9214e1ae68c520a520ff2ce
Rowan Weiffenbach <rowan.weiffenbach@icloud.com>
2025-01-29 15:14:50 +0100

    added subscription id

--- 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
+++ 03-Terraform-Command-Basics/terraform-manifests/azure-resource-group.tf
@@ -12,10 +12,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = "8a026ede-4c46-4018-9c70-53de20c37d19"
 }
 
 # Create Resource Group 
 resource "azurerm_resource_group" "my_demo_rg1" {
-  location = "eastus"
+  location = "westeurope"
   name = "my-demo-rg1"  
 }
